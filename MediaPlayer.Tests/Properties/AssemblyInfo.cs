6a46d9ffe5359442dc87bddb743937eaa2b27bcd
jbath <jaspreetbath98@gmail.com>
2026-04-01 22:03:41 -0700

    Made changes to files so that they run on x64 arch and made a test proj that simulates the VLC libraries to test the initialization, mute button, and trackbar for volume scroll.
    Also had to make the form internal members usable for tests class.

---
+++ MediaPlayer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MediaPlayer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MediaPlayer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8dc28d9f-7c8d-4c95-a782-adc20bf009ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MediaPlayer.Tests/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibVLCSharp.Shared;
+using MediaPlayer;
+
+namespace MediaPlayer.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Form1 _form;
+
+        // Runs exactly once before the test suite starts
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            // crashes if called more than once per process.
+            Core.Initialize();
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _form = new Form1();
+
+            
+            var dummyHandle = _form.Handle;
+        }
+
+        [TestMethod]
+        public void Constructor_InitializesVlcComponents()
+        {
+            Assert.IsNotNull(_form._libVLC,
+                "LibVLC core should be initialized.");
+            Assert.IsNotNull(_form._mediaPlayer,
+                "MediaPlayer instance should be initialized.");
+        }
+
+        [TestMethod]
+        public void BtnMute_Click_TogglesMuteState()
+        {
+            _form.Show();
+
+            // Loading test media to make sure media is valid
+            using (var dummyMedia = new Media(_form._libVLC,
+                new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")))
+            {
+                _form._mediaPlayer.Play(dummyMedia);
+
+                // allowing audio to stabilize
+                System.Threading.Thread.Sleep(500);
+
+
+                _form._mediaPlayer.Mute = false;
+                bool initialMuteState = _form._mediaPlayer.Mute;
+
+
+                MethodInfo muteMethod = typeof(Form1).GetMethod("btnMute_Click",
+                    BindingFlags.NonPublic | 
+                    BindingFlags.Instance);
+
+                muteMethod.Invoke(_form, new object[] { null, EventArgs.Empty });
+
+
+                Assert.AreNotEqual(initialMuteState, _form._mediaPlayer.Mute, 
+                    "The mute state did not toggle correctly.");
+
+
+                _form._mediaPlayer.Stop();
+            }
+        }
+
+
+        [TestMethod]
+        public void TrackBarVolume_Scroll_UpdatesMediaPlayerVolume()
+        {
+            TrackBar mockTrackBar = new TrackBar();
+
+            mockTrackBar.Maximum = 100;
+            mockTrackBar.Value = 75;
+
+            MethodInfo volumeMethod = typeof(Form1).GetMethod("trackBarVolume_Scroll",
+                BindingFlags.NonPublic |
+                BindingFlags.Instance);
+
+            volumeMethod.Invoke(_form, new object[] { mockTrackBar, EventArgs.Empty });
+
+            Assert.AreEqual(75, _form._mediaPlayer.Volume,
+                "The media player volume was not updated to the trackbar value.");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // clean LibVLC unmanaged resources first
+            if (_form != null)
+            {
+                _form._mediaPlayer?.Dispose();
+                _form._libVLC?.Dispose();
+                _form.Dispose();
+            }
+        }
+    }
+}
